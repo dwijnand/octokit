a7a304eab7e79a383187ea9a316ee39db03321ee
Dale Wijnand <dale.wijnand@gmail.com>
2015-05-25 23:20:49 +0100

    @inline all the things!

--- src/main/scala/net/mox9/skithub/package.scala
+++ src/main/scala/net/mox9/skithub/package.scala
@@ -5,27 +5,27 @@ import scala.language.implicitConversions
 import play.api.libs.json.{ JsObject, JsValue }
 
 package object skithub {
-  type ->[+A, +B]     = scala.Product2[A, B]
-  type ?=>[-A, +B]    = scala.PartialFunction[A, B]
-  type Duration       = scala.concurrent.duration.Duration
-  type FiniteDuration = scala.concurrent.duration.FiniteDuration
-  type Future[+T]     = scala.concurrent.Future[T]
-  type JsonFormat[T]  = play.api.libs.json.Format[T]
-  type JsError        = play.api.libs.json.JsError
-  type JsResult[+T]   = play.api.libs.json.JsResult[T]
-  type JsSuccess[T]   = play.api.libs.json.JsSuccess[T]
+  @inline type ->[+A, +B]     = scala.Product2[A, B]
+  @inline type ?=>[-A, +B]    = scala.PartialFunction[A, B]
+  @inline type Duration       = scala.concurrent.duration.Duration
+  @inline type FiniteDuration = scala.concurrent.duration.FiniteDuration
+  @inline type Future[+T]     = scala.concurrent.Future[T]
+  @inline type JsonFormat[T]  = play.api.libs.json.Format[T]
+  @inline type JsError        = play.api.libs.json.JsError
+  @inline type JsResult[+T]   = play.api.libs.json.JsResult[T]
+  @inline type JsSuccess[T]   = play.api.libs.json.JsSuccess[T]
 
-  val ->         = scala.Product2
-  val Future     = scala.concurrent.Future
-  val Json       = play.api.libs.json.Json
-  val JsonFormat = play.api.libs.json.Format
-  val JsError    = play.api.libs.json.JsError
-  val JsSuccess  = play.api.libs.json.JsSuccess
+  @inline val ->         = scala.Product2
+  @inline val Future     = scala.concurrent.Future
+  @inline val Json       = play.api.libs.json.Json
+  @inline val JsonFormat = play.api.libs.json.Format
+  @inline val JsError    = play.api.libs.json.JsError
+  @inline val JsSuccess  = play.api.libs.json.JsSuccess
 
-  implicit def DurationInt(n: Int): scala.concurrent.duration.DurationInt =
+  @inline implicit def DurationInt(n: Int): scala.concurrent.duration.DurationInt =
     scala.concurrent.duration.DurationInt(n)
 
-  implicit class AnyW[T](private val x: T) extends AnyVal {
+  @inline implicit class AnyW[T](private val x: T) extends AnyVal {
     @inline def toUnit(): Unit = ()
 
     @inline def pipe[U](f: T => U): U     = f(x)
@@ -37,16 +37,16 @@ package object skithub {
     @inline def maybe[U](pf: T ?=> U): Option[U] = pf lift x
   }
 
-  implicit class FutureW[T](private val f: Future[T]) extends AnyVal {
+  @inline implicit class FutureW[T](private val f: Future[T]) extends AnyVal {
     @inline def result(atMost: Duration = 5.seconds): T = scala.concurrent.Await.result(f, atMost)
   }
 
-  implicit class JsValueW[T](private val json: JsValue) extends AnyVal {
+  @inline implicit class JsValueW[T](private val json: JsValue) extends AnyVal {
     @inline def pp: String        = Json prettyPrint json
     @inline def toJsonStr: String = Json stringify json
   }
 
-  implicit class JsErrorW[T](private val e: JsError) extends AnyVal {
+  @inline implicit class JsErrorW[T](private val e: JsError) extends AnyVal {
     @inline def toFlatJson: JsObject = JsError toFlatJson e
   }
 }
