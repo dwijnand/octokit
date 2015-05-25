0c1bc817862e2344b28acd8bd0d4796b007dcfc2
Dale Wijnand <dale.wijnand@gmail.com>
2015-05-25 17:05:28 +0100

    Add Future/Duration helpers to package object.

--- src/main/scala/net/mox9/skithub/package.scala
+++ src/main/scala/net/mox9/skithub/package.scala
@@ -1,4 +1,20 @@
 package net.mox9
 
+import scala.language.implicitConversions
+
+import scala.{ concurrent => sc }
+import scala.concurrent.{ duration => scd }
+
 package object skithub {
+  type Duration       = scd.Duration
+  type FiniteDuration = scd.FiniteDuration
+  type Future[+T]     = sc.Future[T]
+
+  val Future = scala.concurrent.Future
+
+  implicit def DurationInt(n: Int): scd.DurationInt = scd.DurationInt(n)
+
+  implicit class FutureWithResult[T](private val f: Future[T]) extends AnyVal {
+    @inline def result(atMost: Duration = 5.seconds): T = sc.Await.result(f, atMost)
+  }
 }
