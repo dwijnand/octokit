37105bdd7e3ed76097cead4a7aad9873a55dd09a
Dale Wijnand <dale.wijnand@gmail.com>
2015-05-25 18:24:28 +0100

    Add try/finally for Play.stop & read repos.

--- src/main/scala/net/mox9/skithub/T.scala
+++ src/main/scala/net/mox9/skithub/T.scala
@@ -1,33 +1,62 @@
 package net.mox9.skithub
 
-import play.api.{ Mode, Play, DefaultApplication }
 import play.api.libs.ws._
+import play.api.{ DefaultApplication, Mode, Play }
 
 import java.io.File
 
 object T {
+  // ISO-8601: YYYY-MM-DDTHH:MM:SSZ
+
+  case class AccessToken(value: String) extends AnyVal {
+    override def toString = value
+  }
+
   def main(args: Array[String]): Unit = {
-    val githubToken = sys.env get "GITHUB_API_TOKEN" getOrElse (sys error "Need to set GITHUB_API_TOKEN")
-    val org = args.head
+    val accessToken =
+      sys.env get "GITHUB_API_TOKEN" getOrElse (sys error "Need to set GITHUB_API_TOKEN") pipe AccessToken
+    val org = args.headOption getOrElse (sys error "Provide an org name")
 
     val appBaseDir = new File(".")
     val appClassLoader = this.getClass.getClassLoader
-    implicit val app = new DefaultApplication(appBaseDir, appClassLoader, None, Mode.Dev) doto Play.start
+
+    Play.start(new DefaultApplication(appBaseDir, appClassLoader, None, Mode.Dev))
+
+    try go(accessToken, org)
+    finally Play.stop()
+  }
+
+  def go(accessToken: AccessToken, org: String): Unit = {
+    import Play.current
 
     val reposFut = (WS
       url s"https://api.github.com/orgs/$org/repos"
       withHeaders        "Accept" ->  "application/vnd.github.v3+json"
-      withHeaders "Authorization" -> s"token $githubToken"
+      withHeaders "Authorization" -> s"token $accessToken"
       get()
     )
 
     val reposResp = reposFut.result()
 
     val json = reposResp.json
-    // ISO-8601: YYYY-MM-DDTHH:MM:SSZ
 
-    json.pp >>
+    val repos = json.validate[Seq[Repo]] recoverTotal
+      (e => sys error s"Failed to read repos:\n${e.toFlatJson.pp}")
 
-    Play.stop()
+    s"${repos.length} repos".>>
+    repos foreach (_.>>)
+
+    ()
   }
+}
+
+case class Repo(
+  name      : String,
+  full_name : String,
+  `private` : Boolean,
+  fork      : Boolean,
+  language  : String
+)
+object Repo {
+  implicit val jsonFormat: JsonFormat[Repo] = Json.format[Repo]
 }
