fe2be11bdbd8816cec4cf387498108a194f2a587
Dale Wijnand <dale.wijnand@gmail.com>
2015-05-26 02:04:03 +0100

    Move all to GitHubClient/etc setup.

--- src/main/scala/net/mox9/skithub/T.scala
+++ src/main/scala/net/mox9/skithub/T.scala
@@ -1,6 +1,7 @@
 package net.mox9.skithub
 
 import play.api.Play.current
+import play.api.libs.json.JsResultException
 //import play.api.libs.concurrent.Execution.Implicits._
 import play.api.libs.ws.WS
 import play.api.{ DefaultApplication, Mode, Play }
@@ -19,62 +20,66 @@ final case class AccessToken(value: String) extends AnyVal with Credentials {
 }
 
 // TODO: Use Credentials
-final class GitHubClient(userAgent: UserAgent, accessToken: AccessToken)
+case class ConnectionConfig(userAgent: UserAgent, accessToken: AccessToken)
 
-object T {
-  def main(args: Array[String]): Unit = {
-    val userAgent = "dwijnand"
-    val accessToken = sys.env get "GITHUB_API_TOKEN"  map AccessToken getOrElse
-      (sys error "Need to set GITHUB_API_TOKEN")
-    val org = args.headOption getOrElse (sys error "Provide an org name")
+final class GitHubClient(connectionConfig: ConnectionConfig) {
+  val orgs = new OrgsClient(connectionConfig)
+}
 
-    val appBaseDir = new File(".")
-    val appClassLoader = this.getClass.getClassLoader
-
-    Play start new DefaultApplication(appBaseDir, appClassLoader, None, Mode.Dev)
-
-    try go(userAgent, accessToken, org)
-    finally Play.stop()
-  }
-
-  def go(userAgent: String, accessToken: AccessToken, org: String): Unit = {
-    val urlStr = s"https://api.github.com/orgs/$org/repos"
-
-    val repos: Seq[Repo] = getRepos(userAgent, accessToken, urlStr).await30s
-
-    s"${repos.length} repos".>>
-//    repos foreach (_.>>)
-
-    ()
-  }
-
-  def getRepos(userAgent: String, accessToken: AccessToken, urlStr: String): Future[Seq[Repo]] = (
-    getRepos1(userAgent, accessToken, urlStr, Vector.empty)
-      map (_ recoverTotal (e => sys error s"Failed to read repos:\n${e.toFlatJson.pp}"))
+final class OrgsClient(connectionConfig: ConnectionConfig) {
+  def getRepos(org: String): Future[Seq[Repo]] = (
+    getRepos1(connectionConfig, s"https://api.github.com/orgs/$org/repos", Vector.empty)
+      flatMap {
+        case JsSuccess(repos, _) => Future successful repos
+        case JsError(errors)     => Future failed JsResultException(errors)
+      }
   )
 
-  def getRepos1(
-    userAgent: String, accessToken: AccessToken, urlStr: String, repos: Vector[Repo]
+  private def getRepos1(
+    connectionConfig: ConnectionConfig, urlStr: String, repos: Vector[Repo]
   ): Future[JsResult[Seq[Repo]]] =
     (WS
       url urlStr
-      withHeaders    "User-Agent" -> userAgent
+      withHeaders    "User-Agent" -> s"${connectionConfig.userAgent}"
       withHeaders        "Accept" ->  "application/vnd.github.v3+json"
-      withHeaders "Authorization" -> s"token $accessToken"
+      withHeaders "Authorization" -> s"token ${connectionConfig.accessToken}"
       get()
       flatMap { resp =>
-        resp.json.validate[Seq[Repo]] match {
-          case JsSuccess(moreRepos, _) =>
-            resp header "Link" flatMap getNextLink match {
-              case Some(nextUrlStr) => getRepos1(userAgent, accessToken, nextUrlStr, repos ++ moreRepos)
-              case None             => Future successful JsSuccess(repos ++ moreRepos)
-            }
-          case jsE: JsError            => Future successful jsE
-        }
+      resp.json.validate[Seq[Repo]] match {
+        case JsSuccess(moreRepos, _) =>
+          resp header "Link" flatMap getNextLink match {
+            case Some(nextUrlStr) => getRepos1(connectionConfig, nextUrlStr, repos ++ moreRepos)
+            case None             => Future successful JsSuccess(repos ++ moreRepos)
+          }
+        case jsE: JsError            => Future successful jsE
       }
-    )
+    }
+  )
 
-  def getNextLink(link: String) = """<(.+)>; rel="next"""".r findFirstMatchIn link map (_ group 1)
+  private def getNextLink(link: String) = """<(.+)>; rel="next"""".r findFirstMatchIn link map (_ group 1)
+}
+
+object T {
+  def main(args: Array[String]): Unit = {
+    val userAgent = UserAgent("dwijnand")
+    val accessToken = sys.env get "GITHUB_API_TOKEN"  map AccessToken getOrElse
+      (sys error "Need to set GITHUB_API_TOKEN")
+
+    val org = args.headOption getOrElse (sys error "Provide an org name")
+
+    start()
+
+    try {
+      val github = new GitHubClient(ConnectionConfig(userAgent, accessToken))
+      val repos = github.orgs.getRepos(org)
+      repos.await30s pipe (rs => s"${rs.length} repos".>>)
+    }
+    finally Play.stop()
+  }
+
+  private def newApp = new DefaultApplication(new File("."), this.getClass.getClassLoader, None, Mode.Dev)
+
+  private def start() = Play start newApp
 }
 
 // ISO-8601: YYYY-MM-DDTHH:MM:SSZ
