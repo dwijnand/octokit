59a26d74b01615be8ee581caf941cd60f9335697
Dale Wijnand <dale.wijnand@gmail.com>
2015-05-26 11:33:24 +0100

    Label tabular extention types.

--- src/main/scala/net/mox9/skithub/package.scala
+++ src/main/scala/net/mox9/skithub/package.scala
@@ -90,13 +90,13 @@ package skithub {
         Future sequence fs
     }
 
-    @inline implicit class MapW[K, V](private val xs: Traversable[K -> V]) {
+    @inline implicit class MapWithTabular[K, V](private val xs: Traversable[K -> V]) {
       @inline def maxKeyLen = xs.toIterator.map(_._1.toString.length).max
       @inline def tabularkv = xs map (kv => s"%${xs.maxKeyLen}s %s".format(kv._1, kv._2))
       @inline def showkv()  = tabularkv foreach println
     }
 
-    @inline implicit class MultimapW[K, V](private val xs: Traversable[K -> Traversable[V]]) {
+    @inline implicit class MultimapWithTabular[K, V](private val xs: Traversable[K -> Traversable[V]]) {
       @inline def tabularkvs = xs map (kv => s"%${xs.maxKeyLen}s %s".format(kv._1, kv._2.mkString("[", "],[", "]")))
       @inline def showkvs()  = tabularkvs foreach println
     }
