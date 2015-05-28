2c717ba017c33778fa1c99c466cd17a2390a252b
Dale Wijnand <dale.wijnand@gmail.com>
2015-05-28 09:24:04 +0100

    Add more play-json methods/ext methods.

--- src/main/scala/net/mox9/skithub/package.scala
+++ src/main/scala/net/mox9/skithub/package.scala
@@ -3,7 +3,9 @@ package net.mox9
 import scala.language.higherKinds
 import scala.language.implicitConversions
 
-import play.api.libs.json.{ JsObject, JsValue }
+import play.api.data.validation.ValidationError
+import play.api.libs.json.Json.JsValueWrapper
+import play.api.libs.json.{ JsArray, JsNull, JsObject, JsPath, JsValue }
 
 import java.util.concurrent.TimeUnit
 
@@ -242,8 +244,16 @@ package skithub {
     @inline val Reads             = play.api.libs.json.Reads
     @inline val Writes            = play.api.libs.json.Writes
 
+    def jsSuccess[T](x: T): JsResult[T] = JsSuccess(x)
+    def jsError(errors: Seq[(JsPath, Seq[ValidationError])]): JsResult[Nothing] = JsError(errors)
+
     @inline implicit class Any2PlayJsonW[T](private val x: T) {
-      @inline def toJson(implicit W: Writes[T]): JsValue = Json toJson x
+      @inline def toJson(implicit W: Writes[T]): JsValue    = Json toJson x
+      @inline def js(implicit W: Writes[T]): JsValueWrapper = x
+    }
+
+    @inline implicit class Option2PlayJsonW[T](private val x: Option[T]) {
+      @inline def orJsNull(implicit z: Writes[T]): JsValueWrapper = x map (_.js) getOrElse JsNull
     }
 
     @inline implicit class String2PlayJsonW(private val s: String) {
@@ -258,10 +268,19 @@ package skithub {
       @inline def pp: String                      = Json prettyPrint json
       @inline def toJsonStr: String               = Json stringify json
       @inline def fromJson[T: Reads]: JsResult[T] = Json fromJson json
+   // @inline def jsValues: Seq[JsValue]          = json.castToOpt[JsArray].fold(nil[JsValue])(_.value)
     }
 
     @inline implicit class JsErrorW[T](private val e: JsError) {
       @inline def toFlatJson: JsObject = JsError toFlatJson e
+    }
+
+    @inline implicit class FutureJsResultW[T](private val f: Future[JsResult[T]]) {
+      @inline def flatten(implicit ec: ExecCtx): Future[T] =
+        f flatMap {
+          case JsSuccess(ts, _) => ts.future
+          case JsError(errors)  => JsResultException(errors).failFut
+        }
     }
   }
 
