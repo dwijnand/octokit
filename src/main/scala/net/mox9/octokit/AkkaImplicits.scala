967fc6e802d652d5b5ac404b12d0f0f7af6f2ce7
Dale Wijnand <dale.wijnand@gmail.com>
2015-05-30 14:37:25 +0100

    Add AkkaImplicits & TODO.

---
+++ src/main/scala/net/mox9/octokit/AkkaImplicits.scala
@@ -0,0 +1,5 @@
+package net.mox9.octokit
+
+trait AkkaImplicits {
+  type ActorSystem = akka.actor.ActorSystem
+}
--- src/main/scala/net/mox9/octokit/package.scala
+++ src/main/scala/net/mox9/octokit/package.scala
@@ -5,6 +5,8 @@ package net.mox9
 // TODO: Explore github.orgs("org-name").repos  .get?
 // TODO: Make aliases final?
 // TODO: Handle HTTP codes. Abstraction?
+// TODO: Rename XImplicits to XKitPre, including SbtKitPre
 package object octokit extends ScalaImplicits
   with TabularImplicits
+  with AkkaImplicits
   with PlayFunctionalImplicits with PlayJsonImplicits with PlayWsImplicits
