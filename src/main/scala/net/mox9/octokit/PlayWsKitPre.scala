8f692107fd1e392494476b7a95ed786ea81d4128
Dale Wijnand <dale.wijnand@gmail.com>
2015-06-01 10:24:23 +0100

    Add a type aliases to PlayWsKitPre.

--- src/main/scala/net/mox9/octokit/PlayWsKitPre.scala
+++ src/main/scala/net/mox9/octokit/PlayWsKitPre.scala
@@ -4,8 +4,9 @@ import scala.language.higherKinds
 import scala.language.implicitConversions
 
 trait PlayWsKitPre {
-  @inline final type WSResponse = play.api.libs.ws.WSResponse
   @inline final type WSClient   = play.api.libs.ws.WSClient
+  @inline final type WSRequest  = play.api.libs.ws.WSRequest
+  @inline final type WSResponse = play.api.libs.ws.WSResponse
 
   @inline final val WS = play.api.libs.ws.WS
 }
