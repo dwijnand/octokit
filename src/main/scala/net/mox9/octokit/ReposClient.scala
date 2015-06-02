9be51dc9a6e1954b719344d16c96e20fb7d7fac5
Dale Wijnand <dale.wijnand@gmail.com>
2015-06-02 16:57:57 +0100

    Flesh out RepoSummary, including User.

--- src/main/scala/net/mox9/octokit/ReposClient.scala
+++ src/main/scala/net/mox9/octokit/ReposClient.scala
@@ -2,9 +2,125 @@ package net.mox9.octokit
 
 import play.api.libs.functional.syntax._
 
-final case class RepoSummary(name: String, `private`: Boolean, fork: Boolean, language: Option[String])
+import java.time._
+
+final case class User(
+  login               : String,
+  id                  : Int,
+  avatar_url          : Url,
+  gravatar_id         : String,
+  url                 : Url,
+  html_url            : Url,
+  followers_url       : Url,
+  gists_url           : Url,
+  starred_url         : Url,
+  subscriptions_url   : Url,
+  organizations_url   : Url,
+  repos_url           : Url,
+  events_url          : Url,
+  received_events_url : Url,
+  `type`              : String,
+  site_admin          : Boolean
+)
+object User {
+  implicit val jsonFormat: JsonFormat[User] = Json.format[User]
+}
+
+final case class RepoPermissions(admin: Boolean, push: Boolean, pull: Boolean)
+object RepoPermissions {
+  implicit val jsonFormat: JsonFormat[RepoPermissions] = Json.format[RepoPermissions]
+}
+
+final case class RepoSummary(
+  id                : Long,
+  owner             : User,
+  name              : String,
+  full_name         : String,
+  description       : Option[String],
+  `private`         : Boolean,
+  fork              : Boolean,
+
+  url               : Url,
+  html_url          : Url,
+  clone_url         : Url,
+  git_url           : Url,
+  ssh_url           : Url,
+  svn_url           : Url,
+  mirror_url        : Option[Url],
+  homepage          : Option[Url],
+
+  language          : Option[String],
+  forks_count       : Int,
+  stargazers_count  : Int,
+  watchers_count    : Int,
+  size              : Int,
+  default_branch    : String,
+  open_issues_count : Int,
+  has_issues        : Boolean,
+  has_wiki          : Boolean,
+  has_pages         : Boolean,
+  has_downloads     : Boolean,
+  pushed_at         : ZonedDateTime,
+  created_at        : ZonedDateTime,
+  updated_at        : ZonedDateTime,
+  permissions       : RepoPermissions
+)
 object RepoSummary {
-  implicit val jsonFormat: JsonFormat[RepoSummary] = Json.format[RepoSummary]
+  implicit val jsonReads: Reads[RepoSummary] = {
+    val r1 = (
+      (__ \ "id"          ) .read[Long]           and
+      (__ \ "owner"       ) .read[User]           and
+      (__ \ "name"        ) .read[String]         and
+      (__ \ "full_name"   ) .read[String]         and
+      (__ \ "description" ) .readNullable[String] and
+      (__ \ "private"     ) .read[Boolean]        and
+      (__ \ "fork"        ) .read[Boolean]
+    ).tupled
+    val r2 = (
+      (__ \ "url"        ) .read[Url] and
+      (__ \ "html_url"   ) .read[Url] and
+      (__ \ "clone_url"  ) .read[Url] and
+      (__ \ "git_url"    ) .read[Url] and
+      (__ \ "ssh_url"    ) .read[Url] and
+      (__ \ "svn_url"    ) .read[Url] and
+      (__ \ "mirror_url" ) .readNullable[Url] and
+      (__ \ "homepage"   ) .readNullable[Url]
+    ).tupled
+    val r3 = (
+      (__ \ "language"          ) .readNullable[String]  and
+      (__ \ "forks_count"       ) .read[Int]             and
+      (__ \ "stargazers_count"  ) .read[Int]             and
+      (__ \ "watchers_count"    ) .read[Int]             and
+      (__ \ "size"              ) .read[Int]             and
+      (__ \ "default_branch"    ) .read[String]          and
+      (__ \ "open_issues_count" ) .read[Int]             and
+      (__ \ "has_issues"        ) .read[Boolean]         and
+      (__ \ "has_wiki"          ) .read[Boolean]         and
+      (__ \ "has_pages"         ) .read[Boolean]         and
+      (__ \ "has_downloads"     ) .read[Boolean]         and
+      (__ \ "pushed_at"         ) .read[ZonedDateTime]   and
+      (__ \ "created_at"        ) .read[ZonedDateTime]   and
+      (__ \ "updated_at"        ) .read[ZonedDateTime]   and
+      (__ \ "permissions"       ) .read[RepoPermissions]
+    ).tupled
+
+    val r: Reads[RepoSummary] = r1 and r2 and r3 apply { (v1, v2, v3) =>
+        val (id, owner, name, full_name, description, private1, fork) = v1
+        val (url, html_url, clone_url, git_url, ssh_url, svn_url, mirror_url, homepage) = v2
+        val (
+          language, fork_count, stargazers_count, watchers_count, size, default_branch, open_issues_count,
+          has_issues, has_wiki, has_pages, has_downloads, pushed_at, created_at, updated_at, permissions
+        ) = v3
+        RepoSummary(
+          id, owner, name, full_name, description, private1, fork,
+          url, html_url, clone_url, git_url, ssh_url, svn_url, mirror_url, homepage,
+          language, fork_count, stargazers_count, watchers_count, size, default_branch, open_issues_count,
+          has_issues, has_wiki, has_pages, has_downloads, pushed_at, created_at, updated_at, permissions
+        )
+    }
+
+    r
+  }
 }
 
 final case class Repo(name: String)
